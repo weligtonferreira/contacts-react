bda165190bff05e3a3ef89400f795f861acd45e3
Weligton Ferreira <57678361+weligtonferreira@users.noreply.github.com>
2024-05-21 09:45:41 -0300

    :sparkles: feat: create HomePage

---
+++ src/pages/HomePage/index.tsx
@@ -0,0 +1,14 @@
+import Header from '../../components/Header';
+
+import './styles.css';
+
+export default function HomePage() {
+  return (
+    <>
+      <Header />
+      <main>
+        <h1>Home</h1>
+      </main>
+    </>
+  );
+}
