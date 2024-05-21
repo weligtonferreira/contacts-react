6b50a8d957985de689d3c77366653bb0627e6ea6
Weligton Ferreira <57678361+weligtonferreira@users.noreply.github.com>
2024-05-21 09:44:41 -0300

    :recycle: refactor: change App element to HomePage element

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
-import App from './App.tsx';
+import HomePage from './pages/HomePage';
 import './index.css';
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <App />,
+    element: <HomePage />,
   },
 ]);
 
